480ddeaeb3f0ca62928375329d66223426e1ea68
Zachary C. <ColonZachary@gmail.com>
2025-10-08 10:50:16 -0400

    Updated_Folder_Structure
    Updated folder structure for better records

--- MTECH 1201/Week 4/Exercise 1/sketch.js
+++ MTECH 1201/Week 4/Exercise 1/sketch.js
@@ -1,7 +1,28 @@
 function setup() {
   createCanvas(400, 400);
+
+
 }
 
 function draw() {
   background(220);
+
+  if (x > width) {
+    xV = -xV;
+  }
+  if (y > height) {
+    yV = -yV;
+  }
+  if (x < 0) {
+    xV = -xV;
+  }
+  if (y < 0) {
+    yV = -yV;
+  }
+
+  // Update position by velocity each frame (If you keep x alone it will only move in the x direction)
+  x = x + xV;
+  y = y + yV;
+
+  circle(x, y, 20);
 }
